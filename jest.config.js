b49f561ab1c08bf78d604c17bfcfec7214586847
prithwish.samanta <prithwish.samanta@gmail.com>
2024-07-31 01:29:13 +0530

    removing unnecessary module exports from root jst config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 // File: jest.config.js (root directory)
 module.exports = {
-  projects: ['<rootDir>/services/*', '<rootDir>/swift-api', '<rootDir>/utils'],
+  projects: ['<rootDir>/services/*', '<rootDir>/swift-api'],
   collectCoverageFrom: [
     '**/*.js',
     '!**/node_modules/**',
@@ -8,32 +8,4 @@ module.exports = {
   ],
   coverageDirectory: '<rootDir>/coverage',
   testEnvironment: 'node',
-};
-
-// File: services/search-service/jest.config.js
-// (Use similar config for songs-service, popularity-service, and trends-service)
-module.exports = {
-  displayName: 'search-service',
-  testMatch: ['<rootDir>/tests/**/*.test.js'],
-  moduleDirectories: ['node_modules', '<rootDir>/src'],
-  testEnvironment: 'node',
-  coverageDirectory: '<rootDir>/coverage',
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.js'
-  ],
-};
-
-// File: swift-api/jest.config.js
-module.exports = {
-  displayName: 'swift-api',
-  testMatch: [
-    '<rootDir>/tests/unit/**/*.test.js',
-    '<rootDir>/tests/integration/**/*.test.js'
-  ],
-  moduleDirectories: ['node_modules', '<rootDir>/src'],
-  testEnvironment: 'node',
-  coverageDirectory: '<rootDir>/coverage',
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.js'
-  ],
 };
